00e8ede7ba7baad63afd7d1a1fdceb759d4c3cbc
AmanDhimanD <dhiman.1999.aman@gmail.com>
2023-07-07 15:12:21 +0530

    prereadyConnectionMongoDB

---
+++ mongobackend/controllers/UserController.js
@@ -0,0 +1,28 @@
+// controllers/UserController.js
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
---
+++ mongobackend/routes/index.js
@@ -0,0 +1,9 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+
+router.get('/users', UserController.getAllUsers);
+router.post('/users', UserController.createUser);
+
+module.exports = router;
